0c758b871c4e3e752a0b46f124dfc5a5bbfcf6db
bino <bonoself@gmail.com>
2023-03-13 22:12:09 +0900

    init

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import cors from '@fastify/cors';
+import { Logger } from 'nestjs-pino';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+  app.register(cors, {
+    origin: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      disableErrorMessages: false,
+    }),
+  );
+  app.useLogger(app.get(Logger));
+  await app.listen(3000, '0.0.0.0');
+}
+bootstrap();
