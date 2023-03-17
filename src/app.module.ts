cc4b123525c0bab72ded63f3600e5c294fce4f06
bino <bonoself@gmail.com>
2023-03-18 01:11:38 +0900

    chore(nestjs-config): add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
+import * as path from 'path';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from 'nestjs-config';
 import { LoggerModule } from 'nestjs-pino';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [LoggerModule.forRoot()],
+  imports: [
+    ConfigModule.load(path.resolve(__dirname, 'config', '**/!(*.d).{ts,js}')),
+    LoggerModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
