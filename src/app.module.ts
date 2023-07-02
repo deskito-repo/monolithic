56e9bb87e6a0ad115eea5cce09b7cf9efe9d7f22
bino <bonoself@gmail.com>
2023-07-02 19:02:55 +0900

    fix:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import * as path from 'path';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from 'nestjs-config';
-import { LoggerModule } from 'nestjs-pino';
 import { AppController } from './app.controller';
 
 @Module({
   imports: [
-    }),
+    ConfigModule.load(path.resolve(__dirname, 'config', '**/!(*.d).{ts,js}')),
   ],
   controllers: [AppController],
 })
